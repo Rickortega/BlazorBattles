d75d9c863bbbf7d135553269cd52c0751e6191f2
Rick Ortega <rickortega@live.com>
2020-11-13 12:17:37 -0600

    Interface Service and Build page

---
+++ BlazorApp1/Client/Services/IUnitService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BlazorApp1.Shared;
+
+namespace BlazorApp1.Client.Services
+{
+    public interface IUnitService
+    {
+        IList<Unit> Units { get; }
+        IList<UserUnit> MyUnits { get; set; }
+        void AddUnits( int unitId );
+    }
+}
